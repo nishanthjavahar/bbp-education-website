7cb0b9e8b247f121d14989ce2516db5b23038a4f
Admin <admin@MacBookPro.lan>
2026-02-28 21:46:17 +0530

    Configured Flask-Mail settings

--- app.py
+++ app.py
@@ -452,8 +452,8 @@ app.config.update(
     MAIL_SERVER=os.getenv("MAIL_SERVER"),
     MAIL_PORT=int(os.getenv("MAIL_PORT", 587)),
     MAIL_USE_TLS=True,
-    MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
-    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
+    MAIL_USERNAME=os.getenv("nishanthbbp@gmail.com"),
+    MAIL_PASSWORD=os.getenv("xmfmmburdoqiypkt"),
 )
 
 mail = Mail(app)
